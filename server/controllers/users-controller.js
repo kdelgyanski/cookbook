1f919a87f6896114cbd503d9b7165a46db61d8a0
kdelgyanski <krrasimir@gmail.com>
2021-11-29 23:53:54 +0100

    utilized userSchema in users-controller

--- server/controllers/users-controller.js
+++ server/controllers/users-controller.js
@@ -1,55 +1,62 @@
-const { v4: uuid } = require('uuid');
-
 const HttpError = require('../models/HttpError');
+const User = require('../models/user');
 
-const USERS = [
-    {
-        id: 1,
-        username: 'cookie',
-        email: 'cookie@test.com',
-        password: 'c00ki3',
-    },
-    {
-        id: 2,
-        username: 'master_chef',
-        email: 'master@test.com',
-        password: 'm@ster',
-    },
-    {
-        id: 3,
-        username: 'homemadeMarmalade',
-        email: 'marmalade@test.com',
-        password: 'marmalade123',
+const getAll = async (req, res, next) => {
+
+    let users;
+    try {
+        users = await User.find({}, '-password');
+    } catch (err) {
+        return next(new HttpError('Something went wrong! Could not get users!', 500));
     }
-]
 
-const getAll = (req, res, next) => {
-    res.status(200).json(USERS);
+    res.status(200).json(users.map(u => u.toObject({ getters: true })));
 };
 
-const signUp = (req, res, next) => {
-    const newUser = {
-        id: uuid(),
+const signUp = async (req, res, next) => {
+
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ username: req.body.username });
+    } catch (err) {
+        return next(new HttpError('Something went wrong! Could not sign up!', 500));
+    }
+
+    if (existingUser) {
+        return next(new HttpError('Could not sign up! User already exists!', 422));
+    }
+
+    const newUser = new User({
         username: req.body.username,
         email: req.body.email,
         password: req.body.password
-    };
+    });
 
-    if (USERS.find(u => u.email === newUser.email || u.username === newUser.username)) {
-        return next(new HttpError('Could not sign up. User with this username or email already exists.', 422));
-    } else {
-        USERS.push(newUser);
+    try {
+        await newUser.save();
+    } catch (err) {
+        return next(new HttpError('Something went wrong! Could not sign up!', 500));
     }
 
     res.status(200).json({ message: 'Successfully signed up with user: ' + newUser.username })
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
 
     const { username, password } = req.body;
-    const user = USERS.find(u => u.username === username);
 
-    if (!user || user.password !== password) {
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ username: username });
+    } catch (err) {
+        return next(new HttpError('Something went wrong! Could not login! Please try again later!', 500));
+    }
+
+    if (!existingUser) {
+        return next(new HttpError('Could not login! User with username: ' + username + ' does not exist!', 401));
+    }
+
+    if (existingUser.password !== password) {
         return next(new HttpError('Could not login. Invalid credentials.', 401));
     }
 
