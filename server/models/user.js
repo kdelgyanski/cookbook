d616a33a82e08a971084c860ce5af3e089b7853f
kdelgyanski <krrasimir@gmail.com>
2021-11-30 00:29:23 +0100

    added recipes to user schema

--- server/models/user.js
+++ server/models/user.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 8 },
     email: { type: String, required: true, unique: true },
+    recipes: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Recipe' }]
 });
 
 userSchema.plugin(uniqueValidator);
