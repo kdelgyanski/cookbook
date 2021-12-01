c70efe80b1499b9975c6ec7bcd3be62e96933b0b
kdelgyanski <krrasimir@gmail.com>
2021-12-01 23:50:31 +0100

    added endpoints for getting recipes by id and by authorId

--- server/controllers/recipes-controller.js
+++ server/controllers/recipes-controller.js
@@ -71,6 +71,46 @@ const getAll = async (req, res, next) => {
     res.status(200).json(recipes.map(r => r.toObject({ getters: true })));
 };
 
+const getById = async (req, res, next) => {
+
+    const recipeId = req.params.recipeId;
+
+    let recipe;
+
+    try {
+        recipe = await Recipe.findById(recipeId);
+    } catch (err) {
+        return next(new HttpError('Something went wrong! Could not get recipe!', 500));
+    }
+
+    if (!recipe) {
+        return next(new HttpError('Could not find recipe with id: ' + recipeId, 404));
+    }
+
+    res.status(200).json(recipe.toObject({ getters: true }));
+
+};
+
+const getByAuthorId = async (req, res, next) => {
+
+    const authorId = req.params.userId;
+
+    let recipes;
+
+    try {
+        recipes = await Recipe.find({ authorId: authorId });
+    } catch (err) {
+        return next(new HttpError('Something went wrong! Could not get recipies!', 500));
+    }
+
+    if (!recipes || recipes.length === 0) {
+        return next(new HttpError('Could not find any recipes for user with id: ' + authorId, 404));
+    }
+
+    res.status(200).json(recipes.map(r => r.toObject({ getters: true })));
+
+};
+
 const create = async (req, res, next) => {
 
     const authorId = req.body.authorId;
@@ -116,4 +156,6 @@ const create = async (req, res, next) => {
 };
 
 exports.getAll = getAll;
+exports.getById = getById;
+exports.getByAuthorId = getByAuthorId;
 exports.create = create;
--- server/routes/recipes-routes.js
+++ server/routes/recipes-routes.js
@@ -2,12 +2,18 @@ const express = require('express');
 
 const {
     getAll,
+    getById,
+    getByAuthorId,
     create
 } = require('../controllers/recipes-controller');
 
 const router = express.Router();
 
 router.get('/', getAll);
+
+router.get('/:recipeId', getById);
+
+router.get('/user/:userId', getByAuthorId);
 
 router.post('/', create);
 
