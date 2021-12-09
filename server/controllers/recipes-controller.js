9a1582997f576486d366aa34a0483d9d727bd4e8
kdelgyanski <krrasimir@gmail.com>
2021-12-10 00:05:59 +0100

    server: added filter for recipes (seasonal, category, course and difficulty)

--- server/controllers/recipes-controller.js
+++ server/controllers/recipes-controller.js
@@ -4,66 +4,13 @@ const HttpError = require('../models/http-error');
 const User = require('../models/user');
 const Recipe = require('../models/recipe');
 
-const RECIPES = [
-    {
-        id: 1,
-        title: 'Pumpkin Pie',
-        author: 1,
-        timeToCook: '30min',
-        preparationTime: '40min',
-        seasonal: ['Autumn'],
-        difficulty: 'Intermediate',
-        type: 'Dessert',
-        category: ['Sweet'],
-        servingPortions: 8,
-        ingredients: [
-            { name: 'pumpkin', quantity: '400', units: 'gram' },
-            { name: 'flour', quantity: '300', units: 'gram' },
-            { name: 'sugar', quantity: '300', units: 'gram' },
-            { name: 'milk', quantity: '100', units: 'ml' },
-            { name: 'vanilla', quantity: '10', units: 'gram' },
-            { name: 'walnuts', quantity: '100', units: 'gram' }
-        ],
-        steps: [
-            { number: 1, description: 'bla bla bla 1' },
-            { number: 2, description: 'bla bla bla 2' },
-            { number: 3, description: 'bla bla bla 3' },
-            { number: 4, description: 'bla bla bla 4' },
-            { number: 5, description: 'bla bla bla 5' },
-        ]
-    },
-    {
-        id: 2,
-        title: 'Baked Salmon',
-        author: 3,
-        timeToCook: '20min',
-        preparationTime: '20min',
-        seasonal: ['Spring', 'Summer'],
-        difficulty: 'Easy',
-        type: 'Main',
-        category: ['Fish', 'Mediterranean'],
-        servingPortions: 1,
-        ingredients: [
-            { name: 'salmon', quantity: '125', units: 'gram' },
-            { name: 'potatoes', quantity: '200', units: 'gram' },
-            { name: 'salt', quantity: '1', units: 'tbsp' },
-            { name: 'pepper', quantity: '1', units: 'tbsp' },
-            { name: 'lemon', quantity: '1', units: 'pc' }
-        ],
-        steps: [
-            { number: 1, description: 'bla bla bla 1' },
-            { number: 2, description: 'bla bla bla 2' },
-            { number: 3, description: 'bla bla bla 3' }
-        ]
-    }
-]
-
-
 const getAll = async (req, res, next) => {
+
+    const findParams = prepareFindParams(req.query);
 
     let recipes;
     try {
-        recipes = await Recipe.find({});
+        recipes = await Recipe.find(findParams);
     } catch (err) {
         return next(new HttpError('Something went wrong! Could not get recipies!', 500));
     }
@@ -156,6 +103,31 @@ const create = async (req, res, next) => {
     res.status(201).json(recipe.toObject({ getters: true }));
 
 };
+
+const prepareFindParams = queryParams => {
+
+    let findParams = {};
+
+    if (queryParams.course) {
+        findParams.course = queryParams.course;
+    }
+
+    if (queryParams.difficulty) {
+        findParams.difficulty = queryParams.difficulty;
+    }
+
+    if (queryParams.seasonal) {
+        findParams.seasonal = queryParams.seasonal;
+    }
+
+    if (queryParams.category) {
+        findParams.category = queryParams.category;
+    }
+
+    return findParams;
+
+};
+
 
 exports.getAll = getAll;
 exports.getById = getById;
